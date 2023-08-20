940cbfa59761a57ed58bb8f30bd1ce3aa6011958
Naomi Lago <naomilago6@gmail.com>
2023-08-20 03:13:14 +0000

    initial settings

---
+++ config/config.py
@@ -0,0 +1,15 @@
+import os
+
+ENV = os.getenv('ENV', 'dev')
+
+SOLUTION = 'news_classification'
+AUTHOR = 'Naomi Lago'
+VERSION = 1.0
+
+EXPERIMENTS = '../assets/experiments/'
+
+PROJECT_PATHS = dict({
+  "experiment": os.path.join(EXPERIMENTS, SOLUTION),
+})
+
+print(PROJECT_PATHS) 
---
+++ config/constants.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from config.config import *
+import os
+
+DATA_INPUTS = dict({
+  
+})
+
+DATA_PROCESS = dict({
+  
+})
+
+DATA_OUTPUTS = dict({
+
+})
+
+EXPERIMENTS_CONSTANTS = dict({
+    "news_classifier": {
+        "description": "Records metrics and params from train/test results",
+        "key_var": "metrics_recording",
+        "model_name": "Newery",
+        "path": os.path.join(
+            PROJECT_PATHS["EXPERIMENT_SOLUTION_PATH"], "topic_modeling_v2"
+        ),
+    }
+})
+
+SOLUTIONS_CONSTANTS = dict({
+  'today_date': datetime.today().strftime('%Y-%m-%d'),
+})
